b33f2b44e0e6dfac07703709c312c3f04a9dfe3a
Robert Witt <robert.witt@zalando.de>
2018-12-25 21:02:57 +0100

    Initial Commit

---
+++ Nearby Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nearby Me
+//
+//  Created by Robert Witt on 25.12.18.
+//  Copyright © 2018 Robert Witt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
