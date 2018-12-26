9bb618aa06ecfe29d31594c4ed4a719a39097f7e
Robert Witt <robert.witt@zalando.de>
2018-12-26 09:24:34 +0100

    Locate user in landmark view controller.

--- Nearby Me/Controller/LandmarkViewController.swift
+++ Nearby Me/Controller/LandmarkViewController.swift
@@ -15,6 +15,15 @@ class LandmarkViewController: UIViewController, CLLocationManagerDelegate {
     //MARK: - Properties & Outlets
     
     private let locationManager = CLLocationManager()
+    
+    private var networkActivityEnabled: Bool {
+        get {
+            return UIApplication.shared.isNetworkActivityIndicatorVisible
+        }
+        set(networkActivityEnabled) {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = networkActivityEnabled
+        }
+    }
 
     @IBOutlet weak var mapView: MKMapView!
     
@@ -53,7 +62,20 @@ class LandmarkViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     private func startLocatingUser() {
-        
+        if CLLocationManager.locationServicesEnabled() {
+            networkActivityEnabled = true
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = 0
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    private func showAlertWithError(_ error: Error) {
+        let alert = UIAlertController(title: "Error",
+                                      message: error.localizedDescription,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        self.present(alert, animated: true)
     }
     
 }
@@ -70,6 +92,24 @@ extension LandmarkViewController {
         default:
             return
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let region = MKCoordinateRegion(center: location.coordinate,
+                                            latitudinalMeters: 1000,
+                                            longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+            mapView.showsUserLocation = true
+            networkActivityEnabled = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        networkActivityEnabled = false
+        showAlertWithError(error)
     }
     
 }
