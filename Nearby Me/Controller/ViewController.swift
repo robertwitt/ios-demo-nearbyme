8511c646e71b9bb806be965cc2c4e2c59f30c794
Robert Witt <robert.witt@zalando.de>
2018-12-26 08:30:31 +0100

    Build view with map, constraints and hooks into view controller.

--- Nearby Me/Controller/ViewController.swift
+++ Nearby Me/Controller/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func locateTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func refreshMapTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
-
