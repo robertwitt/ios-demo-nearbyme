20dbd813bdd37a6bd5b47755b1a1423f3355c6b8
Robert Witt <robert.witt@zalando.de>
2018-12-26 22:27:23 +0100

    Display landmark detail as Wikipedia article in a web view.

--- Nearby Me/Controller/LandmarkDetailViewController.swift
+++ Nearby Me/Controller/LandmarkDetailViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 import WebKit
 
-class LandmarkDetailViewController: UIViewController {
+class LandmarkDetailViewController: UIViewController, WKNavigationDelegate {
     
     //MARK: Properties & Outlets
-    var landmark: Landmark?
+    var landmark: Landmark? {
+        didSet {
+            self.title = landmark?.title
+        }
+    }
 
     @IBOutlet weak var webView: WKWebView!
     
@@ -20,14 +24,38 @@ class LandmarkDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        startLoadingLandmarkDetail()
     }
     
     //MARK: Actions
 
     @IBAction func doneTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
+    }
+    
+    //MARK: Manage Landmark Detail
+    
+    func startLoadingLandmarkDetail() {
+        if let landmarkUrl = landmark?.url {
+            networkActivityEnabled = true
+            webView.navigationDelegate = self
+            webView.load(URLRequest(url: landmarkUrl))
+        }
+    }
+    
+}
+
+//MARK: Web Navigation Delegate
+
+extension LandmarkDetailViewController {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        networkActivityEnabled = false
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        networkActivityEnabled = false
+        showAlertWithError(error)
     }
     
 }
